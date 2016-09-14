704ca180ccd5a6c6bf3eb63b0f769b5de689ef31
Anwar Mamat <anwar@umd.edu>
2016-09-14 11:37:43 -0400

    person class

--- ruby/Ruby_Lecture_4/person.rb
+++ ruby/Ruby_Lecture_4/person.rb
@@ -25,22 +25,3 @@ puts s.city_state_zip
 puts ContactInfo.contact
 
 
-
-===========================
-module ContactInfo
-	attr_accessor :first_name, :last_name, :city, :state, :zip_code
-	NAME="Alice"
-	def full_name
-		return @first_name + ' ' + last_name
-	end
-	
-	def city_state_zip
-		csz = @city
-		csz += @state if @state
-		csz += @zip_code if @zip_code
-		return csz
-	end
-	def ContactInfo.contact
-		return NAME
-	end
-end
