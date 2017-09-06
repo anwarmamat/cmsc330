099703a955fcd05bd70cd362a09294224cea7376
Anwar Mamat <anwar@umd.edu>
2017-09-06 09:50:25 -0400

    adds regex example

---
+++ ruby/Ruby_Lecture_3_Regexp/regexp.rb
@@ -0,0 +1,26 @@
+# Test the pattersn
+# Lists all words from "war and peace" that matched the pattern
+
+pattern = "^b[a-z]*s$"    # starts with b and ends with s
+file="war_peace.txt"
+fp = open(file)
+matched_list={}
+
+n = 20    #number of words before stop matching
+
+while (line =  fp.gets) && matched_list.size < n do
+  words = line.split(/[<>\/,?!.\s'\-()":;*]/)  #split each line into a words array
+  words.each{|word| 
+    word = word.downcase 
+    if(word.match(/#{pattern}/) )
+      matched_list[word]=true
+      break if matched_list.size >  n
+    end
+    
+    }
+end
+
+matched_list.keys.each{|w|
+  puts w
+}
+puts "\nEnd of List"
