8210810cde53b660b07a267a4b4737f3c34505af
Anwar Mamat <anwar@umd.edu>
2018-04-09 15:36:16 -0400

    misc update

--- java/MyList/MyList.java
+++ java/MyList/MyList.java
@@ -36,10 +36,13 @@ public class MyList {
     else return (this.tl()).map(f).cons (f.eval (this.hd()));
   }
 
+
   public int fold (Function<Integer,Function<Integer,Integer>> f, int a) {
     if (this.isNull()) return a;
     else return (this.tl()).fold(f, f.eval(a).eval(this.hd()));
-  } 
+  }
+
+  
 
   public String toString () {
     if (this.isNull()) return "[]";
@@ -53,8 +56,27 @@ public class MyList {
 	System.out.println(l);
 	MyList m = l.map(new AddOne()); // makes list [4;3;2] == 4::3::2::[]
 	System.out.println(m);
+	
 	Function<Integer,Integer> f = (x) -> x + 2;
+	
+	
 	MyList n = l.map(f); // Java 8 syntax!
 	System.out.println(n);
+	
+	
+	
+	Function<Integer, Function<Integer,Integer> > add =  (x)->(y) -> x+y;
+	
+	int sum = l.fold(add,0); // Java 8 syntax!
+	System.out.println("sum=" + sum);
+	
+	
+	
+	Function<Integer, Function<Integer,Integer> > mul =  (x)->(y) -> x*y;
+	
+	int p = l.fold(mul,1); // Java 8 syntax!
+	System.out.println("Prod=" + p);
+	
+	
     }
 }
