20418b8a39b7e23c5c7db346ab6455ff8c8bb508
Anwar Mamat <anwarmamat@gmail.com>
2021-02-22 20:36:27 -0500

    adds qcheck examples

---
+++ ocaml/qcheck/del/del.ml
@@ -0,0 +1,26 @@
+#require "qcheck";;
+
+open QCheck
+
+let rec delete x l =
+  match l with 
+    [] -> []
+  | (y::ys) -> if x == y then ys 
+               else y::(delete x ys)
+             
+let prop_delete x l =
+   not (List.mem x (delete x l))
+
+let test_delete =
+  Test.make
+    ~count:1000 
+    ~name:"delete_test" 
+    (pair small_int (list small_int)) 
+    (fun (x,l)-> prop_delete x l)
+ ;;
+ 
+          
+              QCheck_runner.run_tests ~verbose:true [test_delete];;
+
+
+                                                             
---
+++ ocaml/qcheck/rev/rev.ml
@@ -0,0 +1,20 @@
+#require "qcheck";;
+
+open QCheck
+
+let reverse = List.rev;;
+
+let prop_reverse l = reverse (reverse l) = l;;
+
+let test =
+  Test.make
+  ~count:1000 
+  ~name:"reverse_test" 
+  QCheck.(list small_int) 
+ (fun x-> prop_reverse x)
+;;
+
+ QCheck_runner.run_tests ~verbose:true [test];;
+
+
+                                                             
