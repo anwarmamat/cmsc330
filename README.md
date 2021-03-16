e645081b5a53c2de20afebd2d3dc511c1edb3fb0
Anwar Mamat <anwarmamat@gmail.com>
2021-03-16 11:12:43 -0400

    misc update

--- README.md
+++ README.md
@@ -1,26 +1,4 @@
-# cmsc330 Spring 2016 
+# CMSC330 Code Examples
 
-Ruby 1 Ruby_lecture_1_classes_and_objects.txt
-
-Ruby 2 Ruby_Lecture_2/Ruby_lecture_Arrays.txt
-
-Ruby 3 Regexp
-
-Ruby 4 Mixins, Procs, Code blocks
-
-OCaml 1 Basics
-
-OCaml 2 Higher Order Funtions
-
-OCaml 3 Closures
-
-OCaml 4 Modules, Data types
-
-Polymorphism
-
-Prolog 1 Basics
-
-Prolog 2  Lists
-       
-Prolog 3  Cut
+This repository inclues the coding examples for CMSC330 the University of Maryland. 
 
---
+++ ocaml/qcheck/occur/occur.ml
@@ -0,0 +1,59 @@
+open OUnit2
+open QCheck
+
+let fold = List.fold_left;;
+
+let contains_elem lst e = 
+  fold (fun a x -> if e = x then true else a) false lst
+
+let is_present lst x = 
+  List.map (fun a -> if a = x then 1 else 0) lst
+
+  
+let count_occ lst target =
+(*	fold (fun a x -> if x = target then a + 1 else a) 0 lst *)
+  fold (+) 0 (is_present lst target)
+
+let gen_occur =
+  make (
+      
+  let open Gen in
+  small_int >>= (
+    fun n -> list (int_range min_int (n-1)) >>=
+               (fun l1 -> list (int_range (n+1) (max_int-1)) >>=
+                            ( fun l2 -> list (return n) >>=
+                                          fun l3 -> 
+                                          let a = shuffle_l (l1 @  l2 @ l3) in
+                                          let b = return (List.length l3) in 
+                                        triple a b (return n) 
+                                          
+  )))
+    )
+let test_count_occ2 =
+  Test.make
+ ~name:"test_count_occ2"
+ ~count:1000
+ (gen_occur)
+ (fun (lst, count, item) -> count_occ lst item = count )
+
+;;
+
+(* Tests for count_occ *)
+let test_count_occ = Test.make
+ ~name:"test_count_occ"
+ ~count:1000
+ (int)
+ (fun n->
+   let l1 = Gen.generate1 (list (int_range (min_int+1) (n-1))).gen in
+   let l2 = Gen.generate1 (list (int_range (n+1) (max_int-1))).gen in
+   let l3 = Gen.generate1 (list (always n)).gen in
+   let lst = l1@l2@l3 in
+   let l = Gen.generate1 (Gen.shuffle_l lst) in 
+   count_occ l n = List.length l3 
+)
+let suite =
+  "public" >::: [
+    QCheck_runner.to_ounit2_test test_count_occ
+  ]
+
+let _ = run_test_tt_main suite
